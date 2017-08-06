a6d9f8c5bad7edc145abd1f8c03a19646b184d66
jebin007 <jevinsubedi@gmail.com>
2017-08-06 18:55:50 -0500

    Initial commit -Initial commit with just navigation bar based on google design -virtualenv on googlep folder

---
+++ googlep/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jevin\documents\pycharmprojects\thinkster-django-angular-boilerplate-release\googlep\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
