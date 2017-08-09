bf2dbcb1c2c74e1c9399c3e63f92091b914b77eb
jebin007 <jevinsubedi@gmail.com>
2017-08-09 12:11:24 -0500

    Setting up Angular JS modules: -update the thinkster module to include it's new dependencies.

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,13 @@
-angular
-  .module('thinkster', []);
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster', [
+            'thinkster.routes',
+            'thinkster.authentication'
+        ]);
+
+    angular
+        .module('thinkster.routes', ['ngRoute']);
+
+})();
