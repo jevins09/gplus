36361538e6120018a9d270eedcbbb7567ce07f05
jebin007 <jevinsubedi@gmail.com>
2017-08-10 15:30:19 -0500

    Layout Modules: -Define new thinkster.layout and thinkster.layout.controllers modules

---
+++ static/javascripts/layout/layout.module.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jevin on 8/10/2017.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.layout', [
+            'thinkster.layout.controllers'
+        ]);
+
+    angular
+        .module('thinkster.layout.controllers', []);
+    })();
