ace611427ba9162381ffef93ddc6ded2a9aee660
jebin007 <jevinsubedi@gmail.com>
2017-08-10 15:02:15 -0500

    Controlling the navigation bar with NavbarController: -create a file in static/javacripts/layout/controllers/navbar.controller.js

---
+++ static/javascripts/layout/controllers/navbar.controller.js
@@ -0,0 +1,25 @@
+/**
+ * Created by jevin on 8/10/2017.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.layout.controllers')
+        .controller('NavbarController', NavbarController);
+
+    NavbarController.$inject = ['$scope', 'Authentication'];
+
+    function NavbarController($scope, Authentication) {
+        var vm = this;
+
+        vm.logout = logout;
+
+        function logout() {
+            Authentication.logout();
+        }
+    }
+
+
+})();
