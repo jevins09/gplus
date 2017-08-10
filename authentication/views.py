4c42c391514cde5f874c41bdef4982cc89eec45d
jebin007 <jevinsubedi@gmail.com>
2017-08-10 01:44:50 -0500

    Making the login API view: -make a view called LoginView in authentication/views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,16 @@
 from rest_framework import permissions, viewsets
-from rest_framework import status
 from rest_framework.response import Response
 
 from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
+
+import json
+
+from django.contrib.auth import authenticate, login
+
+from rest_framework import status, views
+
 
 # Create your views here.
 class AccountViewSet(viewsets.ModelViewSet):
@@ -30,6 +36,35 @@ class AccountViewSet(viewsets.ModelViewSet):
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
         return Response({
-            'status' : 'Bad Request',
-            'message' : 'Account could not be created with received data!'
-        }, stauts=status.HTTP_400_BAD_REQUEST)
+            'status': 'Bad Request',
+            'message': 'Account could not be created with received data!'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(views.APIview):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
