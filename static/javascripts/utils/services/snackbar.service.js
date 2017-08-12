2a2c274f12164724cd167436df8ae775aff02ab9
jebin007 <jevinsubedi@gmail.com>
2017-08-11 23:16:53 -0500

    Making a Snackbar service:
    -make a Snackbar service.

---
+++ static/javascripts/utils/services/snackbar.service.js
@@ -0,0 +1,35 @@
+/**
+ * Created by jevin on 8/11/2017.
+ */
+
+(function ($, _) {
+    'use strict';
+
+    angular
+        .module('thinkster.utils.services')
+        .factory('Snackbar', Snackbar);
+
+    function Snackbar() {
+        var Snackbar = {
+            error: error,
+            show: show
+        };
+
+        return Snackbar;
+
+    function _snackbar(content, options) {
+        options = _.extend({ timeout: 3000 }, options);
+        options.content = content;
+
+        $.snackbar(options);
+        }
+
+    function error(content, options) {
+        _snackbar('Error: ' + content, options);
+    }
+
+    function show(content, options) {
+        _snackbar(content, options);
+         }
+    }
+})($, _);
