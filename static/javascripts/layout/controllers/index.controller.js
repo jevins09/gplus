e79a65ab4e76a4877193d895df4485397448ad59
jebin007 <jevinsubedi@gmail.com>
2017-08-11 23:41:25 -0500

    Controlling the index interface with IndexController
    -make a new controller called IndexController in static/javascripts/layout/controllers/index.controller.js
    
    -include index.controller.js in javascripts.html

---
+++ static/javascripts/layout/controllers/index.controller.js
@@ -0,0 +1,42 @@
+/**
+ * Created by jevin on 8/11/2017.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.layout.controllers')
+        .controller('IndexController', IndexController);
+
+    IndexController.$inject = ['$scope', 'Authentication', 'Posts', 'Snackbar'];
+
+    function IndexController($scope, Authentication, Posts, Snackbar) {
+        var vm = this;
+
+        vm.isAuthenticated = Authentication.isAuthenticated();
+        vm.posts = [];
+
+        activate();
+
+        function activate() {
+            Posts.all().then(postsSuccessFn, postsErrorFn);
+
+            $scope.$on('post.created', function (event, post) {
+                vm.posts.unshift(post);
+            });
+
+            $scope.$on('post.created.error', function () {
+                vm.posts.shift();
+            });
+
+            function postsSuccessFn(data, status, headers, config) {
+                vm.posts = data.data;
+            }
+
+            function postsErrorFn(data, status, headers, config) {
+                Snackbar.error(data.error);
+            }
+        }
+    }
+})();
