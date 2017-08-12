f84769f4c7dbd75305b30e6e1ac406ec9a3bc0e0
jebin007 <jevinsubedi@gmail.com>
2017-08-11 23:20:23 -0500

    Making a Snackbar service:
    -Set up your modules for the thinkster.utils module.

---
+++ static/javascripts/utils/utils.module.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jevin on 8/11/2017.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.utils', [
+            'thinkster.utils.services'
+    ]);
+
+    angular
+        .module('thinkster.utils.services', []);
+})();
