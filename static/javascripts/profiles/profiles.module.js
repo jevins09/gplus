1a151dda47085e81a44afd278f431a7b71d56607
jebin007 <jevinsubedi@gmail.com>
2017-08-16 23:08:05 -0500

    Making the profile modules:
    -define the modules needed for profiles in profiles.module.js

---
+++ static/javascripts/profiles/profiles.module.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jevin on 8/16/2017.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.profiles', [
+            'thinkster.profiles.controllers',
+            'thinkster.profiles.services'
+        ]);
+
+    angular
+        .module('thinkster.profiles.controllers', []);
+
+    angular
+        .module('thinkster.profiles.services', []);
+})();
